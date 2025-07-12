3f9f3a5ac61f831fdcd6fd6277d51d62483553b6
Tan Truong <tantruong215@gmail.com>
2025-07-12 03:05:01 -0400

    Add src/pid.h PID interface

---
+++ src/pid.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+class PID {
+public:
+    PID(double kp, double ki, double kd);
+    double compute(double setpoint, double measurement, double dt);
+
+private:
+    double Kp;
+    double Ki;
+    double Kd;
+    double integral;
+    double prev_error;
+};
