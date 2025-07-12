23a931a80bc02f30677805c06970f44f4cfb0386
Tan Truong <tantruong215@gmail.com>
2025-07-12 03:07:13 -0400

    Add src/plant_sim.h plant simulation interface

---
+++ src/plant_sim.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class Plant {
+public:
+    Plant(double ambient, double tau);
+    double read() const;
+    void step(double input, double dt);
+
+private:
+    double T;       // current temperature
+    double ambient; // ambient temperature
+    double tau;     // time constant
+};
