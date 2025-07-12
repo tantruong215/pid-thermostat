c469f6f284f8a17c08be96817ff42567f56b514f
Tan Truong <tantruong215@gmail.com>
2025-07-12 03:05:37 -0400

    Add src/plant_sim.cpp plant simulation implementation

---
+++ src/plant_sim.cpp
@@ -0,0 +1,14 @@
+﻿#include \"plant_sim.h\"
+
+Plant::Plant(double ambient_, double tau_)
+    : T(ambient_), ambient(ambient_), tau(tau_) {}
+
+double Plant::read() const {
+    return T;
+}
+
+void Plant::step(double input, double dt) {
+    // first-order response: dT/dt = (input - (T - ambient)) / tau
+    double dT = (input - (T - ambient)) / tau;
+    T += dT * dt;
+}
